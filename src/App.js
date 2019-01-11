407b53d2b2ac7cf3b09e2d595e84ad9aded9446f
Nutod <alukodotun@gmail.com>
2019-01-11 14:23:07 +0100

    Added Logo Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
 import "circular-std";
+import Logo from "./components/elements/Logo/Logo";
 
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
 				<h1>Application Sample</h1>
+				<p>Paragraph text</p>
+				<Logo />
 			</div>
 		);
 	}
---
+++ src/components/elements/Logo/Logo.js
@@ -0,0 +1,10 @@
+import React from "react";
+import LogoImage from "../../../assets/images/Logo.png";
+
+export default function Logo() {
+	return (
+		<div>
+			<img src={LogoImage} alt="Logo" />
+		</div>
+	);
+}
