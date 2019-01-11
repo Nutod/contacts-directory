c85b31955c037603153d41ff23fc58248b243701
Nutod <alukodotun@gmail.com>
2019-01-11 22:54:09 +0100

    Added new styles to Button Component

--- src/components/elements/Button/Button.js
+++ src/components/elements/Button/Button.js
@@ -10,4 +10,13 @@ export const Button = styled.button`
 	background: linear-gradient(to right, ${theme.brand}, ${theme.purpleLight});
 	border-radius: 3rem;
 	cursor: pointer;
+	box-shadow: 0 5px 10px ${theme.lightGreyOne};
+	transition: all 0.2s;
+	backface-visibility: hidden;
+
+	&:hover {
+		background: linear-gradient(to right, ${theme.purpleLight}, ${theme.brand});
+		transform: translateY(-0.1rem);
+		box-shadow: 0 10px 20px ${theme.lightGreyOne};
+	}
 `;
