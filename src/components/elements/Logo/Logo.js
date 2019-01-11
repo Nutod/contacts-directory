624846c337306f8e3dc8fd84bdd55cee19372f92
Nutod <alukodotun@gmail.com>
2019-01-11 14:25:40 +0100

    Added style sto the Logo Component

--- src/components/elements/Logo/Logo.js
+++ src/components/elements/Logo/Logo.js
@@ -1,10 +1,11 @@
 import React from "react";
 import LogoImage from "../../../assets/images/Logo.png";
+import styled from "styled-components";
+
+const Image = styled.img`
+	width: 8rem;
+`;
 
 export default function Logo() {
-	return (
-		<div>
-			<img src={LogoImage} alt="Logo" />
-		</div>
-	);
+	return <Image src={LogoImage} alt="Logo" />;
 }
