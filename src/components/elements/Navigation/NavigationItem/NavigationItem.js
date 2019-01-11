eaebda945809da8991935bb987f8e7fde3569727
Nutod <alukodotun@gmail.com>
2019-01-11 14:36:41 +0100

    Added styles to the navigation item

--- src/components/elements/Navigation/NavigationItem/NavigationItem.js
+++ src/components/elements/Navigation/NavigationItem/NavigationItem.js
@@ -1,5 +1,19 @@
 import React from "react";
+import styled from "styled-components";
+
+const ListItem = styled.li`
+	list-style: none;
+	margin-right: 2rem;
+	padding: 1rem;
+	cursor: pointer;
+
+	&:hover {
+		color: crimson;
+	}
+`;
+
+// This component will implement links
 
 export default function NavigationItem() {
-	return <li>Navigation Item</li>;
+	return <ListItem>Navigation Item</ListItem>;
 }
