b7a0f3d41e4d9686a100711c3533d2973f3f3e69
Nutod <alukodotun@gmail.com>
2019-01-12 18:05:15 +0100

    Implemented component lazy loading

--- src/container/Layout.js
+++ src/container/Layout.js
@@ -1,26 +1,46 @@
 import React, { Component } from "react";
 import { Switch, Route } from "react-router-dom";
-import SignUp from "../components/auth/SignUp";
-import SignIn from "../components/auth/SignIn";
-import AddNewContact from "../components/contact/AddNewContact";
-import Account from "../components/profile/Account";
-import Profile from "../components/profile/Profile";
-import ContactList from "../components/contact/ContactList";
+import asyncComponent from "../HOCs/asyncComponent/asyncComponent";
 
 // TODO: Default should load  Account if authenticated else, redirect to auth page
 // TODO: Change Icons
 // TODO: Add Responsiveness
+
+const asyncSignUp = asyncComponent(() => {
+	return import("../components/auth/SignUp");
+});
+
+const asyncSignIn = asyncComponent(() => {
+	return import("../components/auth/SignIn");
+});
+
+const asyncAddNewContact = asyncComponent(() => {
+	return import("../components/contact/AddNewContact");
+});
+
+const asyncAccount = asyncComponent(() => {
+	return import("../components/profile/Account");
+});
+
+const asyncProfile = asyncComponent(() => {
+	return import("../components/profile/Profile");
+});
+
+const asynContactList = asyncComponent(() => {
+	return import("../components/contact/ContactList");
+});
+
 export default class Layout extends Component {
 	render() {
 		return (
 			<Switch>
-				<Route path="/auth/sign-up" component={SignUp} />
-				<Route path="/auth/sign-in" component={SignIn} />
-				<Route path="/add-new" component={AddNewContact} />
-				<Route path="/account" component={Account} />
-				<Route path="/profile" component={Profile} />
-				<Route path="/contact-list" component={ContactList} />
-				<Route path="/" exact component={SignIn} />
+				<Route path="/auth/sign-up" component={asyncSignUp} />
+				<Route path="/auth/sign-in" component={asyncSignIn} />
+				<Route path="/add-new" component={asyncAddNewContact} />
+				<Route path="/account" component={asyncAccount} />
+				<Route path="/profile" component={asyncProfile} />
+				<Route path="/contact-list" component={asynContactList} />
+				<Route path="/" exact component={asyncSignIn} />
 			</Switch>
 		);
 	}
