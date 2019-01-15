2be7e270a5f24772faf17f6a37e09b742fa84b02
Nutod <alukodotun@gmail.com>
2019-01-15 23:53:37 +0100

    Added error state to withError Component

--- src/HOCs/withError/withError.js
+++ src/HOCs/withError/withError.js
@@ -1,16 +1,17 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class withError extends Component {
-  state = {
-    error: null
-  }
+	state = {
+		error: null
+	};
 
-  render() {
-    const {children} = this.props
-    return (
-      <div>
-        {children}
-      </div>
-    )
-  }
+	render() {
+		const { children } = this.props;
+
+		if (this.state.error) {
+			return <div>Something went wrong</div>;
+		}
+
+		return <div>{children}</div>;
+	}
 }
