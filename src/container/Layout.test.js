81d117989c296fcef1d55dc5ad8932394eb31318
Nutod <alukodotun@gmail.com>
2019-01-20 17:36:37 +0100

    Added snapshot test to Layout Component

--- src/container/Layout.test.js
+++ src/container/Layout.test.js
@@ -1,10 +1,19 @@
-import Enzyme from "enzyme";
+import React from "react";
+import Enzyme, { shallow } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
+import toJson from "enzyme-to-json";
+import Layout from "./Layout";
 
 Enzyme.configure({ adapter: new Adapter() });
 
-describe("", () => {
-	it("should ", () => {
+describe("<Layout />", () => {
+	const wrapper = shallow(<Layout />);
+	it("Should return default true", () => {
 		expect(true).toBe(true);
+		console.log(wrapper.debug());
+	});
+
+	it("Should match snapshot", () => {
+		expect(toJson(wrapper)).toMatchSnapshot();
 	});
 });
