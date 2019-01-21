07669c5033e1759b146594d169eedfdfef3f315b
Nutod <alukodotun@gmail.com>
2019-01-21 23:45:56 +0100

    Added snapshot test to Profile Component

--- src/components/profile/Profile.test.js
+++ src/components/profile/Profile.test.js
@@ -1,10 +1,18 @@
-import Enzyme from "enzyme";
+import React from "react";
+import Enzyme, { shallow } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
+import toJson from "enzyme-to-json";
+import Profile from "./Profile";
 
 Enzyme.configure({ adapter: new Adapter() });
 
-describe("", () => {
-	it("should ", () => {
+describe("<Profile />", () => {
+	const wrapper = shallow(<Profile />);
+	it("Should return default true", () => {
 		expect(true).toBe(true);
+	});
+
+	it("Should match snapshot", () => {
+		expect(toJson(wrapper)).toMatchSnapshot();
 	});
 });
