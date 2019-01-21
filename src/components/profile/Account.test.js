1db0128deda5caa983ed5c1b59a376c5e092bd3f
Nutod <alukodotun@gmail.com>
2019-01-21 23:45:07 +0100

    Added snapshot test to Account Component

--- src/components/profile/Account.test.js
+++ src/components/profile/Account.test.js
@@ -1,10 +1,18 @@
-import Enzyme from "enzyme";
+import React from "react";
+import Enzyme, { shallow } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
+import toJson from "enzyme-to-json";
+import Account from './Account'
 
 Enzyme.configure({ adapter: new Adapter() });
 
-describe("", () => {
-	it("should ", () => {
+describe("<Account />", () => {
+	const wrapper = shallow(<Account />);
+	it("Should return default true", () => {
 		expect(true).toBe(true);
+	});
+
+	it("Should match snapshot", () => {
+		expect(toJson(wrapper)).toMatchSnapshot();
 	});
 });
