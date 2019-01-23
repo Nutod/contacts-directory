fe83b3b25b645fd0a75a4f567f70f61cebe99fc1
Nutod <alukodotun@gmail.com>
2019-01-23 23:40:19 +0100

    Added snapshot test to Button Component

--- src/components/elements/Button/Button.test.js
+++ src/components/elements/Button/Button.test.js
@@ -1,10 +1,19 @@
-import Enzyme from "enzyme";
+import React from "react";
+import Enzyme, {
+	shallow
+} from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
+import toJson from "enzyme-to-json";
+import Button from './Button'
 
-Enzyme.configure({ adapter: new Adapter() });
+Enzyme.configure({
+	adapter: new Adapter()
+});
 
-describe("", () => {
-	it("should ", () => {
-		expect(true).toBe(true);
+describe("<Button />", () => {
+	const wrapper = shallow( < Button / > );
+
+	it("Should match snapshot", () => {
+		expect(toJson(wrapper)).toMatchSnapshot();
 	});
 });
