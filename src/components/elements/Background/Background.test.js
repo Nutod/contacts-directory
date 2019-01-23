b686956f5185a95940efbf3015a91e25dec355b8
Nutod <alukodotun@gmail.com>
2019-01-23 23:41:17 +0100

    Removed unused test suite

--- src/components/elements/Background/Background.test.js
+++ src/components/elements/Background/Background.test.js
@@ -1,13 +1,17 @@
 import React from "react";
-import Enzyme, { shallow } from "enzyme";
+import Enzyme, {
+  shallow
+} from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 import toJson from "enzyme-to-json";
 import Background from './Background'
 
-Enzyme.configure({ adapter: new Adapter() });
+Enzyme.configure({
+  adapter: new Adapter()
+});
 
 describe("<Background />", () => {
-  const wrapper = shallow(<Background />);
+  const wrapper = shallow( < Background / > );
 
   it("Should match snapshot", () => {
     expect(toJson(wrapper)).toMatchSnapshot();
