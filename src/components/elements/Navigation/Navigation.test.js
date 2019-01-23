ade0495a911a203968e371a74c94d3a12e5d5f0c
Nutod <alukodotun@gmail.com>
2019-01-23 23:45:26 +0100

    Added Navigation Item Component snapshot test

---
+++ src/components/elements/Navigation/Navigation.test.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Enzyme, {
+  shallow
+} from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import toJson from "enzyme-to-json";
+import Navigation from './Navigation'
+
+Enzyme.configure({
+  adapter: new Adapter()
+});
+
+describe("<Navigation />", () => {
+  const wrapper = shallow( < Navigation / > );
+
+  it("Should match snapshot", () => {
+    expect(toJson(wrapper)).toMatchSnapshot();
+  });
+});
