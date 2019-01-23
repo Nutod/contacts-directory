a61078864df7457403cda229dc368807b4266634
Nutod <alukodotun@gmail.com>
2019-01-23 23:39:47 +0100

    Added snapshot test to Background Component

--- src/components/elements/Background/Background.test.js
+++ src/components/elements/Background/Background.test.js
@@ -1,10 +1,15 @@
-import Enzyme from "enzyme";
+import React from "react";
+import Enzyme, { shallow } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
+import toJson from "enzyme-to-json";
+import Background from './Background'
 
 Enzyme.configure({ adapter: new Adapter() });
 
-describe("", () => {
-	it("should ", () => {
-		expect(true).toBe(true);
-	});
+describe("<Background />", () => {
+  const wrapper = shallow(<Background />);
+
+  it("Should match snapshot", () => {
+    expect(toJson(wrapper)).toMatchSnapshot();
+  });
 });
