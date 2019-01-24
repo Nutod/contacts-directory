7019d35efafe05540a8af704da7df9adc44cc29a
Nutod <alukodotun@gmail.com>
2019-01-24 19:42:18 +0100

    Added snapshot test to Sidebar Component

--- src/components/elements/Sidebar/Sidebar.test.js
+++ src/components/elements/Sidebar/Sidebar.test.js
@@ -1,10 +1,19 @@
-import Enzyme from "enzyme";
+import React from "react";
+import Enzyme, {
+	shallow
+} from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
+import toJson from "enzyme-to-json";
+import Sidebar from './Sidebar'
 
-Enzyme.configure({ adapter: new Adapter() });
+Enzyme.configure({
+	adapter: new Adapter()
+});
 
-describe("", () => {
-	it("should ", () => {
-		expect(true).toBe(true);
+describe("<Sidebar />", () => {
+	const wrapper = shallow(< Sidebar />);
+
+	it("Should match snapshot", () => {
+		expect(toJson(wrapper)).toMatchSnapshot();
 	});
 });
